a90ada25bfd64f250dec1ee22ddffff595013ccf
ALI-web242 <alinaqi1351@gmail.com>
2025-07-12 18:37:10 +0500

    favicon replaced

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/chat-logo.png" type="image/png" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-blue-50 dark:bg-gray-900 text-blue-900 dark:text-gray-100`}
       >
